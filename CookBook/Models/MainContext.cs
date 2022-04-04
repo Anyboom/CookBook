84612fdc8a514c0c898857099c8b1c304c38a9fe
Даниил <danilka.volkov.02@mail.ru>
2022-04-04 04:23:39 +0300

    Добавьте файлы проекта.

---
+++ CookBook/Models/MainContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook.Models
+{
+    sealed class MainContext : DbContext
+    {
+        private MainContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Variables.ConnectionString);
+            optionsBuilder.LogTo(message => Debug.WriteLine(message));
+        }
+    }
+}
